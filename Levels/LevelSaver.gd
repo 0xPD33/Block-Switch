0cd00e02d0002619ddddfcc9cc8b25d869bcaeaa
ThePadDev <ThePadDev@gmail.com>
2021-03-11 23:13:26 +0100

    minor fix when saving level
    the root node in LevelTemplate.tscn now has the correct given name

--- Levels/LevelSaver.gd
+++ Levels/LevelSaver.gd
@@ -6,6 +6,7 @@ func save_level(level_name : String):
 	var level = get_parent().get_node("LevelEditorLevel/LevelTemplate")
 	for node in level.get_children():
 		node.owner = level
+	level.name = level_name
 	level_to_save.pack(level)
 	var level_string : String = "res://" + level_name + ".tscn"
 	ResourceSaver.save(level_string , level_to_save)
