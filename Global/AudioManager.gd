f6700992571e666f156f4fa44f3d4ba90c6b3c6a
ThePadDev <ThePadDev@gmail.com>
2021-03-12 16:07:46 +0100

    removed export hints in AudioManager since they are not needed and not usable anymore

--- Global/AudioManager.gd
+++ Global/AudioManager.gd
@@ -1,21 +1,21 @@
 extends Node
 
-export (String, FILE) var interface_click_sound = "res://Assets/SFX/interface_click.wav"
-export (String, FILE) var interface_click_secondary_sound = "res://Assets/SFX/interface_click_secondary.wav"
+var interface_click_sound = "res://Assets/SFX/interface_click.wav"
+var interface_click_secondary_sound = "res://Assets/SFX/interface_click_secondary.wav"
 
-export (String, FILE) var game_over_sound = "res://Assets/SFX/game_over.wav"
-export (String, FILE) var game_retry_sound = "res://Assets/SFX/game_retry.wav"
+var game_over_sound = "res://Assets/SFX/game_over.wav"
+var game_retry_sound = "res://Assets/SFX/game_retry.wav"
 
-export (String, FILE) var place_block_sound = "res://Assets/SFX/add_block.wav"
-export (String, FILE) var delete_block_sound = "res://Assets/SFX/delete_block.wav"
-export (String, FILE) var trash_sound = "res://Assets/SFX/trash_sound.wav"
+var place_block_sound = "res://Assets/SFX/add_block.wav"
+var delete_block_sound = "res://Assets/SFX/delete_block.wav"
+var trash_sound = "res://Assets/SFX/trash_sound.wav"
 
-export (String, FILE) var add_block_sound = "res://Assets/SFX/add_block.wav"
+var add_block_sound = "res://Assets/SFX/add_block.wav"
 
-export (String, FILE) var teleport_sound_1 = "res://Assets/SFX/teleport_1.wav"
-export (String, FILE) var teleport_sound_2 = "res://Assets/SFX/teleport_2.wav"
+var teleport_sound_1 = "res://Assets/SFX/teleport_1.wav"
+var teleport_sound_2 = "res://Assets/SFX/teleport_2.wav"
 
-export (String, FILE) var gem_disappear_sound
+var gem_disappear_sound
 
 var sfx_volume : float = 0
 
