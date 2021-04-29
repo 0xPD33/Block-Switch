9d5b6160beaa0876c1cf6efd6d0e1d0c3f6bb751
ThePadDev <ThePadDev@gmail.com>
2021-04-29 17:16:46 +0200

    reworked save and load systems
    - added a filter that only shows .tscn files
    - saved levels can now be accessed on android devices without problems
    - added a basic condition check to stop one from overwriting an already existing level
    - added an array that keeps track of saved levels

--- Levels/LevelLoadDialog.gd
+++ Levels/LevelLoadDialog.gd
@@ -11,10 +11,12 @@ func _ready():
 	level_editor = get_parent().get_parent()
 	level_editor_panel = level_editor.get_node("UI/LevelEditorPanel")
 	level_editor_options_panel = level_editor.get_node("UI/LevelEditorOptionsPanel")
+	file_dialog.add_filter("*.tscn ; Custom Levels")
 
 
 func open():
 	file_dialog.show()
+	file_dialog.invalidate()
 
 
 func _on_FileDialog_file_selected(path: String):
--- Levels/LevelSaver.gd
+++ Levels/LevelSaver.gd
@@ -1,13 +1,35 @@
 extends Node
 
+var level_string_in_engine : NodePath
+
+var level_saved : bool = false
+var last_level_name : String
+var saved_level_names : Array = []
+
 
 func save_level(level_name : String):
 	var level_to_save : PackedScene = PackedScene.new()
-	var level = get_parent().get_node("LevelEditorLevel/LevelTemplate")
-	for node in level.get_children():
-		node.owner = level
-	level.name = level_name
-	level_to_save.pack(level)
-	var level_string : String = "res://" + level_name + ".tscn"
-	ResourceSaver.save(level_string , level_to_save)
+	var level_string = "user://" + level_name + ".tscn"
+	if check_if_level_exists(level_string) == false:
+		if !level_saved:
+			level_string_in_engine = "LevelEditorLevel/LevelTemplate"
+		else:
+			level_string_in_engine = "LevelEditorLevel/" + last_level_name
+		var level = get_parent().get_node(level_string_in_engine)
+		for node in level.get_children():
+			node.owner = level
+		level.name = level_name
+		level_to_save.pack(level)
+		ResourceSaver.save(level_string , level_to_save)
+		last_level_name = level_name
+		saved_level_names.append(level_name)
+		level_saved = true
+	else:
+		print("level exists already, aborting")
+
+
+func check_if_level_exists(path):
+	var dir = Directory.new()
+	var check_if_exists = dir.file_exists(path)
+	return check_if_exists
 
